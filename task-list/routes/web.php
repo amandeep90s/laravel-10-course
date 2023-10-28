12a4520d8124d2c1f7cccfa8485075baf337ec8f
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-28 11:35:52 +0530

    Blade Templates

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hello World";
+    return view("index", ["name" => "Amandeep Singh"]);
 });
 
 Route::get("/hello", function () {
